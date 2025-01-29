1b8b7820f6983aa8d43985576d12b9576f49522f
aiyiwuzh <aiyiwuzh@usc.edu>
2025-01-28 19:48:33 -0800

    part 1 done

--- split.cpp
+++ split.cpp
@@ -11,13 +11,37 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
-
-/* Add a prototype for a helper function here if you need */
+#include <cstddef>
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  // Base case (the input linked list is empty)
+  if (in == NULL) {
+    return;
+  }
+
+  // Current node's next pointer 
+  Node* in_next = in->next; 
+
+  // Check if current node value is even and add to evens list if so
+  if (in->value % 2 == 0) 
+  {
+    in->next = evens;
+    evens = in;
+  } 
+  // Check if the current node is odd and add to odds list if so
+  else if (in->value % 2 != 0) 
+  {
+    in->next = odds; 
+    odds = in; 
+  }
+
+  // Remove current node from in list 
+  in = NULL; 
+
+  // Recursive call
+  split(in_next, odds, evens);
+
 }
 
-/* If you needed a helper function, write it here */
+
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,80 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
 
+    // TEST CASE 1: 1,2,3,4
+    Node* in = nullptr;
+
+    for (int i = 1; i <= 4; i++) {
+        Node* newNode = new Node(i, nullptr); 
+
+        if (in == nullptr) {
+            in = newNode;
+        }
+        else 
+        {
+            Node* temp = in; 
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+    Node* temp = in; 
+    Node* evens = nullptr;
+    Node* odds = nullptr; 
+
+    split(in, odds, evens);
+
+    std::cout << "Evens: ";
+    temp = evens; 
+    while (temp != nullptr) {
+        std::cout << temp->value << ",";
+        temp = temp->next;
+    }
+
+    std::cout << "Odds: ";
+    temp = odds; 
+    while (temp != nullptr) {
+        std::cout << temp->value << ",";
+        temp = temp->next;
+    }
+    
+    while (odds != nullptr) {
+        Node* temp = odds;
+        odds = odds->next;
+        delete temp;
+    }
+
+    while (evens != nullptr) {
+        Node* temp = evens; 
+        evens = evens->next;
+        delete temp; 
+    }
+
+    // TEST CASE: NULL INPUT
+    Node* in_null = nullptr; 
+    Node* evens_null = nullptr; 
+    Node* odds_null = nullptr; 
+    split(in_null, odds_null, evens_null); 
+
+    std::cout << "Evens: ";
+    temp = evens; 
+    while (temp != nullptr) {
+        std::cout << temp->value << ",";
+        temp = temp->next;
+    }
+
+    std::cout << "Odds: ";
+    temp = odds; 
+    while (temp != nullptr) {
+        std::cout << temp->value << ",";
+        temp = temp->next;
+    }
+
+    return 0; 
 }
