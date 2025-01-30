ba461e3e76ca1f73632b6cf764d5bb2a701fde81
Aiyi Wu Zheng <aiyiwuzh@usc.edu>
2025-01-30 00:10:21 -0800

    part 2 done

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,128 @@
 
 int main(int argc, char* argv[])
 {
+    ULListStr dat; 
 
+    // Push back case 1: empty list 
+    dat.push_back("a");
+    if (dat.size() == 1 && dat.back() == "a")
+    {
+        std::cout << "Push back with empty list success" << std::endl; 
+    }
+
+    // Push back case 2: space exists 
+    dat.push_back("i");
+    if(dat.size() == 2 && dat.back() == "i")
+    {
+        std::cout << "Push back with existing space success" << std::endl;
+    }
+
+    // Push back case 3: not enough space
+    for (int i = 0; i < 8; i++)
+    {
+        dat.push_back(std::to_string(i + 2));
+    }
+    dat.push_back("y");
+    if (dat.size() == 10 && dat.back() == "y")
+    {
+        std::cout << "Push back with not enough space success" << std::endl; 
+    }
+
+    // Push front case 1: empty list
+    dat.clear();
+    dat.push_front("b");
+    if (dat.size() == 1 && dat.front() == "b") {
+        std::cout << "Push front with empty list success" << std::endl;
+    } 
+
+    // Push front case 2: space exists 
+    dat.push_front("e");
+    if(dat.size() == 2 && dat.front() == "e")
+    {
+        std::cout << "Push front with existing space success" << std::endl;
+    }
+
+    // Push front case 3: not enough space
+    for (int i = 0; i < 8; i++)
+    {
+        dat.push_front(std::to_string(i + 2));
+    }
+    dat.push_front("j"); 
+    if (dat.size() == 10 && dat.front() == "j")
+    {
+        std::cout << "Push front with not enough space success" << std::endl; 
+    }
+
+    // Pop back case 1: multiple items
+    dat.clear(); 
+    dat.push_back("q");
+    dat.push_back("w");
+    dat.pop_back(); 
+    if (dat.size() == 1 && dat.front() == "q") {
+        std::cout << "Pop back multi item success" << std::endl; 
+    }
+    
+    // Pop back case 2: 1 item
+    dat.pop_back();
+    if (dat.size() == 0 && dat.empty()) {
+        std::cout << "Pop back one item success" << std::endl;
+    }
+
+    // Pop back case 3: empty list
+    dat.pop_back();
+    if (dat.size() == 0 && dat.empty()) {
+        std::cout << "Pop back empty list success" << std::endl;
+    }
+
+    // Pop front case 1: one item 
+    dat.clear(); 
+    dat.push_back("k");
+    dat.pop_front(); 
+    if (dat.size() == 0 && dat.empty()) {
+        std::cout << "Pop front one item success" << std::endl;
+    }
+
+    // Pop front case 2: multiple items
+    dat.push_back("q");
+    dat.push_back("w");
+    dat.pop_front();
+    if (dat.size() == 1 && dat.front() == "w") {
+        std::cout << "Pop front multi item success" << std::endl; 
+    }
+
+    // Pop front case 3: empty list
+    dat.pop_front();
+    dat.pop_front();
+    if (dat.size() == 0 && dat.empty()) {
+        std::cout << "Pop front empty list success" << std::endl;
+    }
+
+    // Back
+    dat.clear();
+    dat.push_back("t");
+    dat.push_back("p");
+    if (dat.back() == "p") {
+        std::cout << "Back success" << std::endl;
+    }
+
+    // Front
+    dat.clear();
+    dat.push_back("t");
+    dat.push_back("p");
+    if (dat.front() == "t") {
+        std::cout << "Front success" << std::endl;
+    }
+
+    // Get function test
+    dat.clear();
+    dat.push_back("apple");
+    dat.push_back("banana");
+    if (dat.get(0) == "apple" && dat.get(1) == "banana") {
+        std::cout << "Get function success" << std::endl;
+    } else {
+        std::cout << "Get function failed" << std::endl;
+    }
+
+    return 0;
 }
+
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -63,3 +63,164 @@ void ULListStr::clear()
   tail_ = NULL;
   size_ = 0;
 }
+
+// Adds a value to the back of the list 
+void ULListStr::push_back(const std::string& val)
+{
+  // CASE 1: List is empty, make a new item to fill 
+  if (head_ == NULL && tail_ == NULL) 
+  {
+    head_ = new Item();
+    tail_ = head_; 
+    head_->val[0] = val;
+    head_->first = 0; 
+    head_->last = 1;
+  }
+  // CASE 2: There is space in an existing item to add a value at the back
+  else if (tail_->last < ARRSIZE) 
+  {
+    tail_->val[tail_->last] = val; 
+    tail_->last++;
+  }
+  // CASE 3: The tail is full, make a new item to fill and link 
+  else 
+  {
+    // create new item and then insert values
+    Item* newItem = new Item();
+    newItem->val[0] = val; 
+    // set the values for first and last to indicate where this val is
+    newItem->first = 0;
+    newItem->last = 1; 
+
+    // link the item to the previous tail 
+    newItem->prev = tail_;
+    newItem->next = NULL; 
+    tail_->next = newItem;
+    tail_ = newItem; 
+  }
+  // Increment list size 
+  size_++;
+}
+
+// Adds a value to the front of the list
+  void ULListStr::push_front(const std::string& val) 
+  {
+    // CASE 1: List is empty, make a new item to fill 
+    if (head_ == NULL && tail_ == NULL) 
+    {
+      head_ = new Item();
+      tail_ = head_; 
+      head_->val[0] = val;
+      head_->first = 0; 
+      head_->last = 1;
+    }
+    // CASE 2: There is space in existing head item to add a value to the front
+    else if (head_->first > 0)
+    {
+      head_->first--;
+      head_->val[head_->first] = val;
+    }
+    // CASE 3: The head is full, make a new item to fill and link
+    else
+    {
+      Item* newItem = new Item();
+      newItem->val[ARRSIZE-1] = val; 
+      newItem->first = ARRSIZE - 1; 
+      newItem->last = ARRSIZE;
+
+      // link the item to the previous head item
+      newItem->next = head_;
+      head_->prev = newItem; 
+      head_ = newItem; 
+    }
+    // Increment list size
+    size_++;
+  }
+
+  // Removes a value from the back of the list
+  // Assume the list is not empty
+  void ULListStr::pop_back()
+  {
+    // remove the last value in the tail 
+    tail_->last--; 
+
+    // check if the tail becomes empty. if so, delete it
+    if (tail_->first == tail_->last) 
+    {
+      Item* temp = tail_; 
+      tail_ = tail_->prev; 
+      if (tail_) {
+        tail_->next = NULL;
+      }
+      else 
+      {
+        head_ = NULL;
+      }
+      delete temp; 
+    }
+    // decrement size
+    size_--;
+  }
+  
+  // Removes a value from the front of the list
+  void ULListStr::pop_front()
+  {
+    // remove the first value, replace index with next value in list
+    head_->first++;
+
+    // check if the tail becomes empty. if so, delete it
+    if (head_->first == head_->last) 
+    {
+      Item* temp = head_; 
+      head_ = head_->next; 
+      if (head_)
+      {
+        head_->prev = NULL;
+      }
+      else
+      {
+        tail_ = NULL;
+      }
+      delete temp;
+    }
+    // decrement size
+    size_--;
+  }
+
+  // Returns a const reference to the back element
+  std::string const & ULListStr::back() const 
+  {
+    return tail_->val[tail_->last - 1];
+  }
+
+  // Returns a const reference to the front element
+  std::string const & ULListStr::front() const
+  {
+    return head_->val[head_->first];
+  }
+
+  // Returns a pointer to the i-th value in the entire list
+  std::string* ULListStr::getValAtLoc(size_t loc) const
+  {
+    // if non-existent location is given, return NULL
+    if (loc >= size_)
+    {
+      return NULL;
+    }
+
+    // search through the whole list to find desired location
+    Item* currentItem = head_; 
+    while (currentItem != NULL)
+    {
+      // make sure that loc is within the values 
+      if (loc < (currentItem->last - currentItem->first))
+      {
+        return &(currentItem->val[currentItem->first + loc]);
+      }
+      // check the next item for desired loc
+      loc = loc - (currentItem->last - currentItem->first);
+      currentItem = currentItem->next; 
+
+    }
+    return NULL;
+  }
